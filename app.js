fb9e453b1128ac012bda8446ae600f3e484504bb
manasa.muliki <manasa.muliki@xcubelabs.com>
2022-12-16 14:53:59 +0530

    Final version

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 console.log("Hello World")
 console.log('Manasa')
 console.log('We stay in Hyderabad')
+
+const a=2;
+const b=3;
+const sum = a+b;
+console.log('Addition of two numbers', sum)
+
+const c=9;
+const d=3;
+const difference= a-b;
+console.log('Substraction of two numbers', difference)
+
+const e=2;
+const f=3;
+const multiply = a*b;
+console.log('Multiplication of two numbers', multiply)
+
+const g=9;
+const h=3;
+const div = a/b;
+console.log('Divisionof two numbers', div)
